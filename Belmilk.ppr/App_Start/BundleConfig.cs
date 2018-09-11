e2f80e97f67075cd7cf71e429753185dd94bce38
faric91 <a.v.krypsky@gmail.com>
2018-09-11 12:42:11 +0300

    Add project files.

---
+++ Belmilk.ppr/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Belmilk.ppr
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/node_modules/jquery/jquery.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/node_modules/bootstrap/dist/js/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bundle").Include(
+                      "~/dist/bundle.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/node_modules/bootstrap/dist/css/bootstrap.min.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Belmilk.ppr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Belmilk.ppr.MvcApplication" Language="C#" %>
