968ad99449c01764ce24fedef81d18ad6711b3df
faric91 <a.v.krypsky@gmail.com>
2018-09-17 17:03:20 +0300

    2017.09.17-17.03

--- Belmilk.ppr/src/Core.js
+++ Belmilk.ppr/src/Core.js
@@ -2,17 +2,34 @@
 import FirebaseBelmilk from './firebase_belmilk.js'
 import SearchEqpmBelmilk from './search_belmilk.js'
 import _ from 'lodash'
-//import { Promise, Date } from 'core-js';
 
 
 export default function CoreBelmilk() {
-    this.eqpmCompare = function (item, option) {
-        if (item === undefined)
-            return false;
 
-        var date = new Date(item.prevdate);
-        if (new Date(option.startdate) < date && date < new Date(option.enddate)) {
+    function getDate (date, period) {
+        var date = new Date(date);
+        return date.setMonth(date.getMonth() + period);
+    }
+
+    function dateCompare (date, period, option) {
+        var _date = getDate(date, period);
+        if (new Date(option.startdate) < _date && _date < new Date(option.enddate))
             return true;
+        return false;
+    }
+
+    this.eqpmCompare = function (item, option) {
+        if (dateCompare(item.prevdate, item.period, option))
+            return true;
+        else {
+            var counter = 0;
+            item.components.forEach((component) => {
+                if (dateCompare(component.prevdate, component.period, option))
+                    counter++;
+            });
+            if (counter > 0)
+                return true;
+            return false;
         }
     }
 
@@ -21,14 +38,17 @@ export default function CoreBelmilk() {
             var filterEqpms;
             var frDb = new FirebaseBelmilk();
             frDb.init();
-            console.log('0.res: ');
             frDb.equipments().then((eqpms) => {
                 filterEqpms = _.filter(eqpms, (item) => {
+                    if (item === undefined)
+                        return false;
+
                     return this.eqpmCompare(item, option);
                 });
                 res(filterEqpms);
             });
         });
     }
+
 }
 
